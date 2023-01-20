0625b7153a1049108cfae9779a4019ea6ba03e07
Sukwants <95968907+Sukwants@users.noreply.github.com>
2023-01-20 11:05:53 +0800

    0.0.1 Dev.4

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ commander.version(version);
 
 commander
   .command('init [directory]')
-  .description('Initialize a new OI contest.')
+  .description('initialize a new OI contest')
   .action((require('./lib/create.js')).init);
+
+commander
+  .command('new <directory>')
+  .option('-t, --traditional', 'traditional problem')
+  .option('-i, --interactive', 'interactive problem')
+  .option('-c, --communication', 'communication problem')
+  .option('-s, --submitting', 'answer submitting problem')
+  .description('create a new problem (traditional default)')
+  .action((require('./lib/create.js')).new);
 
 commander.parse(process.argv);
